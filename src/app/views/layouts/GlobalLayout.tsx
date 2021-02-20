ba5387415a2a1f9de5a6fc91deb9d86775d40bd8
Squibs <z.r.holman+github@gmail.com>
2021-02-19 21:19:34 -0700

    Happy with page border.

--- src/app/views/layouts/GlobalLayout.tsx
+++ src/app/views/layouts/GlobalLayout.tsx
@@ -17,8 +17,35 @@ interface LayoutProps {
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }: LayoutProps) => {
+  const createSVGs = () => {
+    const svgLong = 'calc(100% - 20px)'; // longer of the two dimensions
+    const svgShort = '10px'; // shorter of the two dimensions | also works as distance from edge
+
+    // 0: top, 1: right, 2: bottom, 3: left
+    const svg = (position: 0 | 1 | 2 | 3) => (
+      <svg
+        width={position === 0 || position === 2 ? svgLong : svgShort}
+        height={position === 0 || position === 2 ? svgShort : svgLong}
+        xmlns="http://www.w3.org/2000/svg"
+        style={{
+          position: 'fixed',
+          top: `${position !== 2 && svgShort}`.replace('false', ''),
+          right: `${position === 1 && svgShort}`.replace('false', ''),
+          bottom: `${position === 2 && svgShort}`.replace('false', ''),
+          left: `${position !== 1 && svgShort}`.replace('false', ''),
+        }}
+      >
+        <rect width="100%" height="100%" rx="5" fill="#BDA453" />
+      </svg>
+    );
+
+    // eslint-disable-next-line prettier/prettier, react/jsx-one-expression-per-line
+    return <div>{svg(0)}{svg(1)}{svg(2)}{svg(3)}</div>; // prettier-ignore
+  };
+
   return (
     <>
+      {createSVGs()}
       <ThemeProvider theme={theme}>{children}</ThemeProvider>
     </>
   );
--- src/app/views/pages/index.tsx
+++ src/app/views/pages/index.tsx
@@ -1,28 +1,43 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Background = styled.div`
+/* --------------------------------- styles --------------------------------- */
+
+const PageContainer = styled.div`
   height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   background-color: ${(props) => props.theme.colors.primaryDark};
   color: ${(props) => props.theme.colors.whiteTint};
 `;
 
+const ContentContainer = styled.div`
+  padding: 40px;
+  text-align: center;
+`;
+
+/* -------------------------------- component ------------------------------- */
+
 const IndexPage: React.FC = () => {
   return (
-    <Background>
-      <h1>Bunch of Text</h1>
-      <h2>Some more text as a sub-header</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque erat ut mauris
-        fringilla imperdiet. Proin quis varius nibh. Vivamus ipsum nibh, consectetur eget elit at,
-        hendrerit viverra neque. Donec non sagittis urna. Duis fringilla sodales libero sit amet
-        tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus
-        mus. Nulla sit amet viverra justo, at ornare erat. Vestibulum ante ipsum primis in faucibus
-        orci luctus et ultrices posuere cubilia curae; Mauris dapibus lectus ultricies, consequat
-        purus non, molestie nunc. Morbi condimentum, velit et accumsan suscipit, neque metus
-        facilisis nunc, in vestibulum urna.
-      </p>
-    </Background>
+    <PageContainer>
+      <ContentContainer>
+        <h1>Bunch of Text</h1>
+        <h2>Some more text as a sub-header</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque erat ut mauris
+          fringilla imperdiet. Proin quis varius nibh. Vivamus ipsum nibh, consectetur eget elit at,
+          hendrerit viverra neque. Donec non sagittis urna. Duis fringilla sodales libero sit amet
+          tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+          ridiculus mus. Nulla sit amet viverra justo, at ornare erat. Vestibulum ante ipsum primis
+          in faucibus orci luctus et ultrices posuere cubilia curae; Mauris dapibus lectus
+          ultricies, consequat purus non, molestie nunc. Morbi condimentum, velit et accumsan
+          suscipit, neque metus facilisis nunc, in vestibulum urna.
+        </p>
+      </ContentContainer>
+    </PageContainer>
   );
 };
 
