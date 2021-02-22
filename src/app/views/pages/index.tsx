9857b2d07117ab375fa81d218613eda2c6d468ac
Squibs <z.r.holman+github@gmail.com>
2021-02-22 14:01:00 -0700

    Scrollbar styles with hiding and showing.

--- src/app/views/pages/index.tsx
+++ src/app/views/pages/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useMemo, useRef } from 'react';
+import { debounce, throttle } from 'lodash';
 import styled from 'styled-components';
 import { PortraitWithBackground } from '../components';
 
@@ -12,24 +13,167 @@ const PageContainer = styled.div`
   align-items: center;
   background-color: ${(props) => props.theme.colors.primaryDark};
   color: ${(props) => props.theme.colors.whiteTint};
+
+  .on-scroll {
+    scrollbar-width: thin;
+    -ms-overflow-styled: none;
+    scrollbar-color: ${(props) => props.theme.colors.accentOne}95
+      ${(props) => props.theme.colors.primaryDark};
+    &::-webkit-scrollbar {
+      width: 11px !important;
+      background-color: ${(props) => props.theme.colors.primaryDark};
+    }
+    &::-webkit-scrollbar-track {
+      box-shadow: none !important;
+      -webkit-box-shadow: none !important;
+      background: ${(props) => props.theme.colors.primaryDark} !important;
+    }
+    &::-webkit-scrollbar-thumb {
+      background-color: ${(props) => props.theme.colors.accentOne}95;
+      border-radius: 6px;
+      border: 3px solid ${(props) => props.theme.colors.primaryDark};
+    }
+  }
 `;
 
 const ContentContainer = styled.div`
-  padding: 0 40px;
   text-align: center;
-  height: 93%;
-  overflow: hidden;
+  width: calc(100% - 43px);
+  height: calc(100% - 40px);
+  overflow-y: auto;
+
+  /* @@@@@@@@@@@@@ scrollbar @@@@@@@@@@@@@ */
+  scrollbar-width: thin;
+  scrollbar-color: transparent transparent;
+  -ms-overflow-style: none;
+  &::-webkit-scrollbar {
+    width: 11px;
+    background-color: transparent;
+  }
+  &::-webkit-scrollbar-track {
+    box-shadow: none;
+    -webkit-box-shadow: none;
+    background-color: transparent;
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: transparent;
+  }
 `;
 
 /* -------------------------------- component ------------------------------- */
 
 const IndexPage: React.FC = () => {
+  const scrollBarRef = useRef() as React.MutableRefObject<HTMLInputElement>;
+  /** Used to remove/hide scrollbar styles after user is done scrolling */
+  const removeStylesMemo = useMemo(
+    () =>
+      debounce(() => {
+        if (scrollBarRef.current.classList.contains('on-scroll')) {
+          scrollBarRef.current.classList.remove('on-scroll');
+        }
+      }, 1000),
+    [],
+  );
+
   return (
     <PageContainer>
-      <ContentContainer>
+      <ContentContainer
+        ref={scrollBarRef}
+        onScroll={throttle(
+          () => {
+            if (!scrollBarRef.current.classList.contains('on-scroll')) {
+              scrollBarRef.current.classList.add('on-scroll');
+            }
+
+            removeStylesMemo();
+          },
+          1000,
+          { trailing: false, leading: true },
+        )}
+      >
         <PortraitWithBackground />
         <h1>Bunch of Text</h1>
         <h2>Some more text as a sub-header</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque erat ut mauris
+          fringilla imperdiet. Proin quis varius nibh. Vivamus ipsum nibh, consectetur eget elit at,
+          hendrerit viverra neque. Donec non sagittis urna. Duis fringilla sodales libero sit amet
+          tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+          ridiculus mus. Nulla sit amet viverra justo, at ornare erat. Vestibulum ante ipsum primis
+          in faucibus orci luctus et ultrices posuere cubilia curae; Mauris dapibus lectus
+          ultricies, consequat purus non, molestie nunc. Morbi condimentum, velit et accumsan
+          suscipit, neque metus facilisis nunc, in vestibulum urna.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque erat ut mauris
+          fringilla imperdiet. Proin quis varius nibh. Vivamus ipsum nibh, consectetur eget elit at,
+          hendrerit viverra neque. Donec non sagittis urna. Duis fringilla sodales libero sit amet
+          tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+          ridiculus mus. Nulla sit amet viverra justo, at ornare erat. Vestibulum ante ipsum primis
+          in faucibus orci luctus et ultrices posuere cubilia curae; Mauris dapibus lectus
+          ultricies, consequat purus non, molestie nunc. Morbi condimentum, velit et accumsan
+          suscipit, neque metus facilisis nunc, in vestibulum urna.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque erat ut mauris
+          fringilla imperdiet. Proin quis varius nibh. Vivamus ipsum nibh, consectetur eget elit at,
+          hendrerit viverra neque. Donec non sagittis urna. Duis fringilla sodales libero sit amet
+          tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+          ridiculus mus. Nulla sit amet viverra justo, at ornare erat. Vestibulum ante ipsum primis
+          in faucibus orci luctus et ultrices posuere cubilia curae; Mauris dapibus lectus
+          ultricies, consequat purus non, molestie nunc. Morbi condimentum, velit et accumsan
+          suscipit, neque metus facilisis nunc, in vestibulum urna.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque erat ut mauris
+          fringilla imperdiet. Proin quis varius nibh. Vivamus ipsum nibh, consectetur eget elit at,
+          hendrerit viverra neque. Donec non sagittis urna. Duis fringilla sodales libero sit amet
+          tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+          ridiculus mus. Nulla sit amet viverra justo, at ornare erat. Vestibulum ante ipsum primis
+          in faucibus orci luctus et ultrices posuere cubilia curae; Mauris dapibus lectus
+          ultricies, consequat purus non, molestie nunc. Morbi condimentum, velit et accumsan
+          suscipit, neque metus facilisis nunc, in vestibulum urna.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque erat ut mauris
+          fringilla imperdiet. Proin quis varius nibh. Vivamus ipsum nibh, consectetur eget elit at,
+          hendrerit viverra neque. Donec non sagittis urna. Duis fringilla sodales libero sit amet
+          tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+          ridiculus mus. Nulla sit amet viverra justo, at ornare erat. Vestibulum ante ipsum primis
+          in faucibus orci luctus et ultrices posuere cubilia curae; Mauris dapibus lectus
+          ultricies, consequat purus non, molestie nunc. Morbi condimentum, velit et accumsan
+          suscipit, neque metus facilisis nunc, in vestibulum urna.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque erat ut mauris
+          fringilla imperdiet. Proin quis varius nibh. Vivamus ipsum nibh, consectetur eget elit at,
+          hendrerit viverra neque. Donec non sagittis urna. Duis fringilla sodales libero sit amet
+          tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+          ridiculus mus. Nulla sit amet viverra justo, at ornare erat. Vestibulum ante ipsum primis
+          in faucibus orci luctus et ultrices posuere cubilia curae; Mauris dapibus lectus
+          ultricies, consequat purus non, molestie nunc. Morbi condimentum, velit et accumsan
+          suscipit, neque metus facilisis nunc, in vestibulum urna.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque erat ut mauris
+          fringilla imperdiet. Proin quis varius nibh. Vivamus ipsum nibh, consectetur eget elit at,
+          hendrerit viverra neque. Donec non sagittis urna. Duis fringilla sodales libero sit amet
+          tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+          ridiculus mus. Nulla sit amet viverra justo, at ornare erat. Vestibulum ante ipsum primis
+          in faucibus orci luctus et ultrices posuere cubilia curae; Mauris dapibus lectus
+          ultricies, consequat purus non, molestie nunc. Morbi condimentum, velit et accumsan
+          suscipit, neque metus facilisis nunc, in vestibulum urna.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque erat ut mauris
+          fringilla imperdiet. Proin quis varius nibh. Vivamus ipsum nibh, consectetur eget elit at,
+          hendrerit viverra neque. Donec non sagittis urna. Duis fringilla sodales libero sit amet
+          tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+          ridiculus mus. Nulla sit amet viverra justo, at ornare erat. Vestibulum ante ipsum primis
+          in faucibus orci luctus et ultrices posuere cubilia curae; Mauris dapibus lectus
+          ultricies, consequat purus non, molestie nunc. Morbi condimentum, velit et accumsan
+          suscipit, neque metus facilisis nunc, in vestibulum urna.
+        </p>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque erat ut mauris
           fringilla imperdiet. Proin quis varius nibh. Vivamus ipsum nibh, consectetur eget elit at,
