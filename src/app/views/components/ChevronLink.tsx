ff7a3e50e4f3af65b7dee1c88362ddcb483b4734
Squibs <z.r.holman+github@gmail.com>
2021-02-24 17:17:57 -0700

    Changed inner-shadow for chevrons.

--- src/app/views/components/ChevronLink.tsx
+++ src/app/views/components/ChevronLink.tsx
@@ -27,19 +27,20 @@ export const ChevronLink = ({
         padding: 0,
         border: 0,
         background: 'transparent',
+        cursor: 'pointer',
       }}
     >
       <svg height="100%" viewBox="0 0 85 240" xmlns="http://www.w3.org/2000/svg">
         <defs>
-          <filter id="inner-shadow-filter-0" x="-500%" y="-500%" width="1000%" height="1000%">
-            <feOffset dx="0" dy="0" />
-            <feGaussianBlur stdDeviation="5" />
-            <feComposite operator="out" in="SourceGraphic" />
-            <feComponentTransfer result="choke">
-              <feFuncA type="linear" slope="1.75" />
+          <filter id="inset-shadow">
+            <feOffset dx="10" dy="10" />
+            <feGaussianBlur stdDeviation="10" result="offset-blur" />
+            <feComposite operator="out" in="SourceGraphic" in2="offset-blur" result="inverse" />
+            <feFlood floodColor="black" floodOpacity="1" result="color" />
+            <feComposite operator="in" in="color" in2="inverse" result="shadow" />
+            <feComponentTransfer in="shadow" result="shadow">
+              <feFuncA type="linear" slope=".75" />
             </feComponentTransfer>
-            <feFlood floodColor="rgba(0,0,0,0.7)" result="color" />
-            <feComposite operator="in" in="color" in2="choke" result="shadow" />
             <feComposite operator="over" in="shadow" in2="SourceGraphic" />
           </filter>
         </defs>
@@ -48,7 +49,7 @@ export const ChevronLink = ({
           id="inner-fill"
           d="M 32.403 120.289 L 73.422 79.166 C 75.317 77.089 76.368 74.377 76.368 71.562 C 76.368 68.747 75.317 66.034 73.422 63.957 C 71.35 62.057 68.644 61.003 65.836 61.003 C 63.028 61.003 60.322 62.057 58.25 63.957 L 9.926 112.966 C 8.031 115.042 6.98 117.755 6.98 120.57 C 6.98 123.385 8.031 126.098 9.926 128.175 L 58.25 176.62 C 60.165 178.641 62.779 179.85 65.555 180 C 66.95 180.039 68.335 179.754 69.602 179.168 C 70.869 178.581 71.984 177.709 72.86 176.62 C 74.755 174.543 75.806 171.83 75.806 169.015 C 75.806 166.2 74.755 163.487 72.86 161.41 L 32.403 120.289 Z"
           fill={fill}
-          style={{ filter: 'url(#inner-shadow-filter-0)' }}
+          style={{ filter: 'url(#inset-shadow)' }}
         />
       </svg>
     </button>
