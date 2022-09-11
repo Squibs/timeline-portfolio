cd611420404db87b9afba8f24fb115520238b49a
Squibs <z.r.holman+github@gmail.com>
2022-09-11 16:26:10 -0600

    List formed, just needs styles.

--- src/app/views/pages/timeline-list.tsx
+++ src/app/views/pages/timeline-list.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import { Link } from 'gatsby';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 import { AppState } from '../../state/store';
@@ -8,6 +9,10 @@ import { BorderContainer } from '../components';
 
 const PageContainer = styled.div`
   background-color: ${({ theme }) => theme.colors.primaryLight};
+
+  & h1 {
+    color: ${({ theme }) => theme.colors.primaryDark};
+  }
 `;
 
 /* ---------------------------------- types --------------------------------- */
@@ -18,18 +23,33 @@ const TimelineList = (): JSX.Element => {
   const { projectsToDisplay } = useSelector(({ timeline: { timeline } }: AppState) => ({
     projectsToDisplay: timeline.projectsToDisplay,
   }));
+  const [timelineListArray, setTimelineListArray] = useState<JSX.Element[]>([]);
 
-  const testProjectFetch = () => {
-    console.log(projectsToDisplay);
-  };
+  // form project list
+  const formProjectList = useCallback(() => {
+    const tArray: React.SetStateAction<JSX.Element[]> = [];
+
+    projectsToDisplay.forEach((project) => {
+      tArray.push(
+        <Link to={`/project/${project.projectLink}`}>
+          <li>{project.title}</li>
+        </Link>,
+      );
+    });
+
+    setTimelineListArray(tArray);
+  }, [projectsToDisplay]);
+
+  // call formProjectList on mount
+  useEffect(() => {
+    formProjectList();
+  }, [formProjectList]);
 
   return (
     <PageContainer className="page-container-styles">
       <BorderContainer />
       <h1>List of Projects on Timeline</h1>
-      <button type="button" onClick={() => testProjectFetch()}>
-        Click ME
-      </button>
+      <ul>{timelineListArray}</ul>
     </PageContainer>
   );
 };
