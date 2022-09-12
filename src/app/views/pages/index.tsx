388430135f5bc0f42ce18ab3fa436a94464b0fdd
Squibs <z.r.holman+github@gmail.com>
2022-09-12 14:23:06 -0600

    Index social links added.

--- src/app/views/pages/index.tsx
+++ src/app/views/pages/index.tsx
@@ -1,6 +1,15 @@
 import React, { useLayoutEffect, useRef } from 'react';
 import styled from 'styled-components';
 import { shallowEqual, useSelector } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import {
+  faGithub,
+  faFreeCodeCamp,
+  faCodepen,
+  faYoutube,
+  faTwitter,
+} from '@fortawesome/free-brands-svg-icons';
 import { BorderContainer, ChevronLink, PortraitWithBackground } from '../components';
 import { Colors } from '../shared';
 import { useScrollHook, useSelectedProjectHook } from '../hooks';
@@ -16,6 +25,9 @@ const ContentContainer = styled.main`
   outline: none;
   overflow-y: scroll;
   color: ${({ theme }) => theme.colors.whiteTint};
+  display: flex;
+  flex-direction: column;
+
   p {
     font-weight: 300;
   }
@@ -31,6 +43,18 @@ const ContentContainer = styled.main`
     right: 20px;
     width: 9px;
   } */
+`;
+
+const LinkContainer = styled.div`
+  margin-top: auto;
+  margin-bottom: 30px;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+
+  & > div {
+    margin-bottom: 10px;
+  }
 `;
 
 /* -------------------------------- component ------------------------------- */
@@ -53,6 +77,44 @@ const IndexPage: React.FC = () => {
     contentContainerRef.current.autofocus = true;
     contentContainerRef.current.focus();
   }, []);
+
+  const customThinCircle = () => {
+    return (
+      <svg
+        className="fa-stack-2x"
+        viewBox="0 0 256 256"
+        id="Flat"
+        xmlns="http://www.w3.org/2000/svg"
+        style={{ color: '#bda453' }}
+      >
+        <path
+          fill="#bda453"
+          d="M128,228A100,100,0,1,1,228,128,100.11316,100.11316,0,0,1,128,228Zm0-192a92,92,0,1,0,92,92A92.1042,92.1042,0,0,0,128,36Z"
+        />
+      </svg>
+    );
+  };
+
+  const fontawesomeHelper = (icon: IconDefinition) => {
+    return (
+      <FontAwesomeIcon
+        icon={icon}
+        className="fa-stack-1x"
+        style={{ color: '#bda453', fontSize: '40px' }}
+      />
+    );
+  };
+
+  const makeLink = (link: string, icon: IconDefinition) => {
+    return (
+      <a href={link}>
+        <span className="fa-stack fa-2x">
+          {customThinCircle()}
+          {fontawesomeHelper(icon)}
+        </span>
+      </a>
+    );
+  };
 
   return (
     <PageContainer className="page-container-styles">
@@ -83,166 +145,23 @@ const IndexPage: React.FC = () => {
           ultricies, consequat purus non, molestie nunc. Morbi condimentum, velit et accumsan
           suscipit, neque metus facilisis nunc, in vestibulum urna.
         </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque erat ut mauris
-          fringilla imperdiet. Proin quis varius nibh. Vivamus ipsum nibh, consectetur eget elit at,
-          hendrerit viverra neque. Donec non sagittis urna. Duis fringilla sodales libero sit amet
-          tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
-          ridiculus mus. Nulla sit amet viverra justo, at ornare erat. Vestibulum ante ipsum primis
-          in faucibus orci luctus et ultrices posuere cubilia curae; Mauris dapibus lectus
-          ultricies, consequat purus non, molestie nunc. Morbi condimentum, velit et accumsan
-          suscipit, neque metus facilisis nunc, in vestibulum urna.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque erat ut mauris
-          fringilla imperdiet. Proin quis varius nibh. Vivamus ipsum nibh, consectetur eget elit at,
-          hendrerit viverra neque. Donec non sagittis urna. Duis fringilla sodales libero sit amet
-          tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
-          ridiculus mus. Nulla sit amet viverra justo, at ornare erat. Vestibulum ante ipsum primis
-          in faucibus orci luctus et ultrices posuere cubilia curae; Mauris dapibus lectus
-          ultricies, consequat purus non, molestie nunc. Morbi condimentum, velit et accumsan
-          suscipit, neque metus facilisis nunc, in vestibulum urna.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque erat ut mauris
-          fringilla imperdiet. Proin quis varius nibh. Vivamus ipsum nibh, consectetur eget elit at,
-          hendrerit viverra neque. Donec non sagittis urna. Duis fringilla sodales libero sit amet
-          tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
-          ridiculus mus. Nulla sit amet viverra justo, at ornare erat. Vestibulum ante ipsum primis
-          in faucibus orci luctus et ultrices posuere cubilia curae; Mauris dapibus lectus
-          ultricies, consequat purus non, molestie nunc. Morbi condimentum, velit et accumsan
-          suscipit, neque metus facilisis nunc, in vestibulum urna.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque erat ut mauris
-          fringilla imperdiet. Proin quis varius nibh. Vivamus ipsum nibh, consectetur eget elit at,
-          hendrerit viverra neque. Donec non sagittis urna. Duis fringilla sodales libero sit amet
-          tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
-          ridiculus mus. Nulla sit amet viverra justo, at ornare erat. Vestibulum ante ipsum primis
-          in faucibus orci luctus et ultrices posuere cubilia curae; Mauris dapibus lectus
-          ultricies, consequat purus non, molestie nunc. Morbi condimentum, velit et accumsan
-          suscipit, neque metus facilisis nunc, in vestibulum urna.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque erat ut mauris
-          fringilla imperdiet. Proin quis varius nibh. Vivamus ipsum nibh, consectetur eget elit at,
-          hendrerit viverra neque. Donec non sagittis urna. Duis fringilla sodales libero sit amet
-          tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
-          ridiculus mus. Nulla sit amet viverra justo, at ornare erat. Vestibulum ante ipsum primis
-          in faucibus orci luctus et ultrices posuere cubilia curae; Mauris dapibus lectus
-          ultricies, consequat purus non, molestie nunc. Morbi condimentum, velit et accumsan
-          suscipit, neque metus facilisis nunc, in vestibulum urna.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque erat ut mauris
-          fringilla imperdiet. Proin quis varius nibh. Vivamus ipsum nibh, consectetur eget elit at,
-          hendrerit viverra neque. Donec non sagittis urna. Duis fringilla sodales libero sit amet
-          tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
-          ridiculus mus. Nulla sit amet viverra justo, at ornare erat. Vestibulum ante ipsum primis
-          in faucibus orci luctus et ultrices posuere cubilia curae; Mauris dapibus lectus
-          ultricies, consequat purus non, molestie nunc. Morbi condimentum, velit et accumsan
-          suscipit, neque metus facilisis nunc, in vestibulum urna.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque erat ut mauris
-          fringilla imperdiet. Proin quis varius nibh. Vivamus ipsum nibh, consectetur eget elit at,
-          hendrerit viverra neque. Donec non sagittis urna. Duis fringilla sodales libero sit amet
-          tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
-          ridiculus mus. Nulla sit amet viverra justo, at ornare erat. Vestibulum ante ipsum primis
-          in faucibus orci luctus et ultrices posuere cubilia curae; Mauris dapibus lectus
-          ultricies, consequat purus non, molestie nunc. Morbi condimentum, velit et accumsan
-          suscipit, neque metus facilisis nunc, in vestibulum urna.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque erat ut mauris
-          fringilla imperdiet. Proin quis varius nibh. Vivamus ipsum nibh, consectetur eget elit at,
-          hendrerit viverra neque. Donec non sagittis urna. Duis fringilla sodales libero sit amet
-          tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
-          ridiculus mus. Nulla sit amet viverra justo, at ornare erat. Vestibulum ante ipsum primis
-          in faucibus orci luctus et ultrices posuere cubilia curae; Mauris dapibus lectus
-          ultricies, consequat purus non, molestie nunc. Morbi condimentum, velit et accumsan
-          suscipit, neque metus facilisis nunc, in vestibulum urna.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque erat ut mauris
-          fringilla imperdiet. Proin quis varius nibh. Vivamus ipsum nibh, consectetur eget elit at,
-          hendrerit viverra neque. Donec non sagittis urna. Duis fringilla sodales libero sit amet
-          tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
-          ridiculus mus. Nulla sit amet viverra justo, at ornare erat. Vestibulum ante ipsum primis
-          in faucibus orci luctus et ultrices posuere cubilia curae; Mauris dapibus lectus
-          ultricies, consequat purus non, molestie nunc. Morbi condimentum, velit et accumsan
-          suscipit, neque metus facilisis nunc, in vestibulum urna.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque erat ut mauris
-          fringilla imperdiet. Proin quis varius nibh. Vivamus ipsum nibh, consectetur eget elit at,
-          hendrerit viverra neque. Donec non sagittis urna. Duis fringilla sodales libero sit amet
-          tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
-          ridiculus mus. Nulla sit amet viverra justo, at ornare erat. Vestibulum ante ipsum primis
-          in faucibus orci luctus et ultrices posuere cubilia curae; Mauris dapibus lectus
-          ultricies, consequat purus non, molestie nunc. Morbi condimentum, velit et accumsan
-          suscipit, neque metus facilisis nunc, in vestibulum urna.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque erat ut mauris
-          fringilla imperdiet. Proin quis varius nibh. Vivamus ipsum nibh, consectetur eget elit at,
-          hendrerit viverra neque. Donec non sagittis urna. Duis fringilla sodales libero sit amet
-          tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
-          ridiculus mus. Nulla sit amet viverra justo, at ornare erat. Vestibulum ante ipsum primis
-          in faucibus orci luctus et ultrices posuere cubilia curae; Mauris dapibus lectus
-          ultricies, consequat purus non, molestie nunc. Morbi condimentum, velit et accumsan
-          suscipit, neque metus facilisis nunc, in vestibulum urna.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque erat ut mauris
-          fringilla imperdiet. Proin quis varius nibh. Vivamus ipsum nibh, consectetur eget elit at,
-          hendrerit viverra neque. Donec non sagittis urna. Duis fringilla sodales libero sit amet
-          tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
-          ridiculus mus. Nulla sit amet viverra justo, at ornare erat. Vestibulum ante ipsum primis
-          in faucibus orci luctus et ultrices posuere cubilia curae; Mauris dapibus lectus
-          ultricies, consequat purus non, molestie nunc. Morbi condimentum, velit et accumsan
-          suscipit, neque metus facilisis nunc, in vestibulum urna.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque erat ut mauris
-          fringilla imperdiet. Proin quis varius nibh. Vivamus ipsum nibh, consectetur eget elit at,
-          hendrerit viverra neque. Donec non sagittis urna. Duis fringilla sodales libero sit amet
-          tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
-          ridiculus mus. Nulla sit amet viverra justo, at ornare erat. Vestibulum ante ipsum primis
-          in faucibus orci luctus et ultrices posuere cubilia curae; Mauris dapibus lectus
-          ultricies, consequat purus non, molestie nunc. Morbi condimentum, velit et accumsan
-          suscipit, neque metus facilisis nunc, in vestibulum urna.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque erat ut mauris
-          fringilla imperdiet. Proin quis varius nibh. Vivamus ipsum nibh, consectetur eget elit at,
-          hendrerit viverra neque. Donec non sagittis urna. Duis fringilla sodales libero sit amet
-          tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
-          ridiculus mus. Nulla sit amet viverra justo, at ornare erat. Vestibulum ante ipsum primis
-          in faucibus orci luctus et ultrices posuere cubilia curae; Mauris dapibus lectus
-          ultricies, consequat purus non, molestie nunc. Morbi condimentum, velit et accumsan
-          suscipit, neque metus facilisis nunc, in vestibulum urna.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque erat ut mauris
-          fringilla imperdiet. Proin quis varius nibh. Vivamus ipsum nibh, consectetur eget elit at,
-          hendrerit viverra neque. Donec non sagittis urna. Duis fringilla sodales libero sit amet
-          tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
-          ridiculus mus. Nulla sit amet viverra justo, at ornare erat. Vestibulum ante ipsum primis
-          in faucibus orci luctus et ultrices posuere cubilia curae; Mauris dapibus lectus
-          ultricies, consequat purus non, molestie nunc. Morbi condimentum, velit et accumsan
-          suscipit, neque metus facilisis nunc, in vestibulum urna.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque erat ut mauris
-          fringilla imperdiet. Proin quis varius nibh. Vivamus ipsum nibh, consectetur eget elit at,
-          hendrerit viverra neque. Donec non sagittis urna. Duis fringilla sodales libero sit amet
-          tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
-          ridiculus mus. Nulla sit amet viverra justo, at ornare erat. Vestibulum ante ipsum primis
-          in faucibus orci luctus et ultrices posuere cubilia curae; Mauris dapibus lectus
-          ultricies, consequat purus non, molestie nunc. Morbi condimentum, velit et accumsan
-          suscipit, neque metus facilisis nunc, in vestibulum urna.
-        </p>
+        <LinkContainer>
+          <div>
+            {makeLink('https://github.com/squibs', faGithub)}
+            {makeLink(
+              'mailto:***REMOVED***?subject=Timeline Portfolio - Contact Request',
+              faEnvelope,
+            )}
+          </div>
+          <div>
+            {makeLink('https://www.freecodecamp.org/squibs', faFreeCodeCamp)}
+            {makeLink('https://codepen.io/Sulph', faCodepen)}
+          </div>
+          <div>
+            {makeLink('https://www.youtube.com/squibsvids', faYoutube)}
+            {makeLink('https://twitter.com/SquibsVids', faTwitter)}
+          </div>
+        </LinkContainer>
       </ContentContainer>
     </PageContainer>
   );
